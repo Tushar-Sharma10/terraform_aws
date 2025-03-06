5586240a2126e5d601b542d020c0faec8ce04498
Tushar Sharma <tusharsharma.techsavvy@gmail.com>
2025-03-06 01:09:31 -0500

    Added Kubernetes provider to provider.tf for cluster management

--- providers.tf
+++ providers.tf
@@ -4,8 +4,24 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.88.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.36.0"
+    }
   }
 }
+
 provider "aws" {
   region = "ca-central-1"
+}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.eks_cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster_auth.token
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+
+}
+
+data "aws_eks_cluster_auth" "cluster_auth" {
+  name = aws_eks_cluster.eks_cluster.name
 }
